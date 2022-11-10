b64ebebf93276baa94a2eb6f55c292d4f1bc6b3a
rachelharu <rachel.haru9@gmail.com>
2022-11-10 14:39:41 -0800

    fix bug in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
-import styles from './style';
-
-import {
-  Navbar,
-  Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testimonials,
-  Hero,
-} from './components';
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}><Navbar/></div>
-    </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}><Hero/>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
         <CardDeal />
-        <Business />
-        <Testimonials /> 
+        <Testimonials />
         <Clients />
         <CTA />
         <Footer />
