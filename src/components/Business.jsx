a112dc9ab0791303444a4e096b0d7fb5492adcbf
rachelharu <rachel.haru9@gmail.com>
2022-11-15 11:22:21 -0800

    create business and fix blue slowdown

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { features } from '../constants';
+import styles, { layout } from '../style';
+import Button from './Button';
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business,
+          <br className='sm:block hidden' /> we'll handle the money.
+        </h2>
+      </div>
+    </section>
+  );
+};
 
-export default Business
+export default Business;
