19c16ba0852b1fdb7f62ec558b1969f0a4928dd7
rachelharu <rachel.haru9@gmail.com>
2022-11-14 18:34:11 -0800

    fix code formatting

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,8 @@ import GetStarted from './GetStarted';
 
 const Hero = () => (
   <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div
-      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div
-        className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient 
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient 
         rounded-[10px] mb-2'>
         <img src={discount} alt='discount' className='w-[32px] h-[32px]' />
         <p className={`${styles.paragraph} ml-2`}>
@@ -17,10 +15,8 @@ const Hero = () => (
       </div>
 
       <div className='flex flex-row justify-between items-center w-full'>
-        <h1
-          className='flex-1 font-poppins font-semibold ss:text-[72px]
-          text-[52px] text-white ss:leading-[100-px] leading-[75px]'
-        >
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px]
+          text-[52px] text-white ss:leading-[100-px] leading-[75px]' >
           The Next <br className='sm:block hidden' />{' '}
           <span className='text-gradient'>Generation</span>{' '}
         </h1>
@@ -28,11 +24,9 @@ const Hero = () => (
           <GetStarted />
         </div>
       </div>
-      <h1
-        className='font-poppins font-semibold ss:text-[68px]
-          text-[52px] text-white ss:leading-[100-px] leading-[75px] w-full'
-      >
-        Payment Method.
+      <h1 className='font-poppins font-semibold ss:text-[68px]
+          text-[52px] text-white ss:leading-[100-px] leading-[75px] w-full'>
+          Payment Method.
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Our team of experts uses a methodology to identify the credit cards most
